6266aa39e19083bb857295f993be3b3b23b079f3
Emily Chen <emily.764537@gmail.com>
2024-04-08 14:25:26 -0700

    added started on problem 27

---
+++ 27.py
@@ -0,0 +1,11 @@
+"""
+Title: 27. Removed Element
+Question Link: https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+Author: Emily Chen
+
+HELP/REFERENCE ACKNOWLEDGEMENT
+Persons:
+Online sources: 
+
+BUGS: None.
+"""
